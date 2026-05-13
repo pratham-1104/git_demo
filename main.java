eb5b2f5b8b40ec53ea0eff4f75114ebcab167d71
Prathmesh <pratham11082004@gmail.com>
2026-05-13 11:18:12 +0530

    Initial Commit

---
+++ main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Welcome to Git Demo Project.");
+        System.out.println("This is a simple Java program.");
+    }
+}
